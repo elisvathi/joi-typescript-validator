2916b59af597c549ee5e3220492d6d88c190a14e
hiavraham <isaakavrahamonov@icloud.com>
2021-12-09 18:01:03 +0100

    clean add types and docs to decorator declarations

--- src/decorators/BaseDecorators.ts
+++ src/decorators/BaseDecorators.ts
@@ -1,5 +1,6 @@
 import Joi, { ValidationOptions } from "joi";
 import "reflect-metadata";
+import { Class } from "../types";
 import { FieldDescription } from "./FieldDescription";
 
 export const MetadataKeys = {
@@ -71,179 +72,198 @@ function setSchemaOptions(target: any, options: ValidationOptions){
 }
 
 /**
- * Marks field as required
- * If overridden by Optional the required flag might be turned off depending which decorations gets called last
+ * Mark field value as required
+ * @template T
  */
-export function Required() {
-    return function(target: any, propertyKey: string) {
-        const description: FieldDescription = { required: true };
-        setFieldDescription(target, propertyKey, description);
-    };
+export function Required<T>() {
+  return (target: T, propertyKey: string) => {
+    const description = { required: true };
+    setFieldDescription(target, propertyKey, description);
+  };
 }
 
 /**
- * Allows the field to have null value
+ * Mark field value as optional
+ * @template T
  */
-export function Nullable(enable = true) {
-    return function(target: any, propertyKey: string) {
-        const description: FieldDescription = { nullable: enable };
-        setFieldDescription(target, propertyKey, description);
-    };
+export function Optional<T>() {
+  return (target: T, propertyKey: string) => {
+    const description = { required: false };
+    setFieldDescription(target, propertyKey, description);
+  };
 }
 
 /**
- * Marks field as optional
- * If overridden by Required the required flag might be turned on depending which decorations gets called last
+ * Mark field value as nullable
+ * @template T
+ * @param {boolean} [isEnabled=true] Flag used to overwrite decorator on parent class field
  */
-export function Optional() {
-    return (target: any, propertyKey: string) => {
-        const description: FieldDescription = { required: false };
-        setFieldDescription(target, propertyKey, description);
-    };
+export function Nullable<T>(isEnabled = true) {
+  return (target: T, propertyKey: string) => {
+    const description = { nullable: isEnabled };
+    setFieldDescription(target, propertyKey, description);
+  };
 }
 
 /**
- *  Used to specify the array type
- * @param tp Array item type  (must be a Class not interface or type since it will be used as a value )
+ * Overwrite automatic field type with the given value
+ * @template T
+ * @template I
+ * @param {Class<I>} type Primitive or class value to set the field type to
  */
-export function ItemType(tp: any) {
-    return (target: any, propertyKey: string) => {
-        const description: FieldDescription = { typeInfo: tp };
-        setFieldDescription(target, propertyKey, description);
-    };
+export function ItemType<T, I>(type: Class<I>) {
+  return (target: T, propertyKey: string) => {
+    const description = { typeInfo: type };
+    setFieldDescription(target, propertyKey, description);
+  };
 }
 
 /**
- * Max value for a number field
- * @param value Number or threshold object
+ * Constrain number field to be less than or equal to a certain value
+ * @template T
+ * @param {Threshold | number} value Value, by which, to constrain the field to be less than or equal to
  */
-export function Max(value: Threshold | number) {
-    return (target: any, propertyKey: string) => {
-        let mValue: Threshold;
-        if (typeof (value) === "number") {
-            mValue = { value: value as number };
-        } else {
-            mValue = value as Threshold;
-        }
-        const description: FieldDescription = { maxValue: mValue };
-        setFieldDescription(target, propertyKey, description);
-    };
+export function Max<T>(value: Threshold | number) {
+  const maxValue = typeof (value) === "number" ? { value } : value;
+
+  return (target: T, propertyKey: string) => {
+    const description = { maxValue };
+    setFieldDescription(target, propertyKey, description);
+  };
 }
 
 /**
- * Max length for a string or array field
- * @param value maxLength
+ * Constrain number field to be greater than or equal to a certain value
+ * @template T
+ * @param {Threshold | number} value Value, by which, to constrain the field to be greater than or equal to
  */
-export function MaxLength(value: number) {
-    return (target: any, propertyKey: string) => {
-        const description: FieldDescription = { maxLength: value };
-        setFieldDescription(target, propertyKey, description);
-    };
+export function Min<T>(value: Threshold | number) {
+  const minValue = typeof (value) === "number" ? { value } : value;
+
+  return (target: T, propertyKey: string) => {
+    const description = { minValue };
+    setFieldDescription(target, propertyKey, description);
+  };
 }
 
 /**
- * Min value for a number field
- * @param value Number or threshold object
+ * Constrain number field to be a positive number (greater than 0)
+ * @template T
+ * @param {boolean} [isEnabled=true] Flag used to overwrite decorator on parent class field
  */
-export function Min(value: Threshold | number) {
-    let mValue: Threshold;
-    if (typeof (value) === "number") {
-        mValue = { value: value as number };
+export function Positive<T>(isEnabled = true) {
+  return (target: T, propertyKey: string) => {
+    const description = { positive: isEnabled };
+    setFieldDescription(target, propertyKey, description);
+  };
+}
+
+/**
+ * Constrain number field to be a negative number (less than 0)
+ * @template T
+ * @param {boolean} [isEnabled=true] Flag used to overwrite decorator on parent class field
+ */
+export function Negative<T>(isEnabled = true) {
+  return (target: T, propertyKey: string) => {
+    const description = { negative: isEnabled };
+    setFieldDescription(target, propertyKey, description);
+  };
+}
+
+/**
+ * Constrain array or string field length to be greater than 0
+ * @template T
+ * @param {boolean} [isEnabled=true] Flag used to overwrite decorator on parent class field
+ */
+export function NotEmpty<T>(isEnabled = true) {
+  return (target: T, propertyKey: string) => {
+    const description = { nonempty: isEnabled };
+    setFieldDescription(target, propertyKey, description);
+  };
+}
+
+/**
+ * Constrain array or string field to have a maximum length
+ * @template T
+ * @param {number} value Value, by which, to constrain the maximum length
+ */
+export function MaxLength<T>(value: number) {
+  return (target: T, propertyKey: string) => {
+    const description = { maxLength: value };
+    setFieldDescription(target, propertyKey, description);
+  };
+}
+
+/**
+ * Constrain array or string field to have a minimum length
+ * @template T
+ * @param {number} value Value, by which, to constrain the minimum length
+ */
+export function MinLength<T>(value: number) {
+  return (target: T, propertyKey: string) => {
+    const description = { minLength: value };
+    setFieldDescription(target, propertyKey, description);
+  };
+}
+/**
+ * Constrain field to only the allowed values passed
+ * @template T
+ * @param {unknown[]} args Values, by which, to constrain the field
+ */
+export function ValidOptions<T>(...args: unknown[]) {
+  return (target: T, propertyKey: string) => {
+    const description = { options: args };
+    setFieldDescription(target, propertyKey, description);
+  };
+}
+
+/**
+ * Constrain field value to be of email format
+ * @template T
+ * @param {boolean} [isEnabled=true] Flag used to overwrite decorator on parent class field
+ */
+export function Email<T>(isEnabled = true) {
+  return (target: T, propertyKey: string) => {
+    const description = { email: isEnabled };
+    setFieldDescription(target, propertyKey, description);
+  };
+}
+
+/**
+ * Constrain date or string field to be of a given format
+ * @template T
+ * @param {string} [format="YYYY-MM-DD"] Format, by which, to constrain the field
+ */
+export function DateString<T>(format = "YYYY-MM-DD") {
+  return (target: T, propertyKey: string) => {
+    const description = { dateString: true, dateStringFormat: format };
+    setFieldDescription(target, propertyKey, description);
+  };
+}
+
+/**
+ * Constrain field or entire class fields by the Joi schema or function passed
+ * @template T
+ * @param {SchemaArgs} schema Joi schema or schema fuction, by which, to constrain field or class
+ */
+export function CustomSchema<T>(schema: SchemaArgs) {
+  return (target: T | Class<T>, propertyKey?: string) => {
+    if (propertyKey) {
+      const description = { customSchema: schema };
+      setFieldDescription(target as T, propertyKey, description);
     } else {
-        mValue = value as Threshold;
+      setSchemaGlobals(target as Class<T>, schema);
     }
-    return function(target: any, propertyKey: string) {
-        const description: FieldDescription = { minValue: mValue };
-        setFieldDescription(target, propertyKey, description);
-    };
+  };
 }
 
 /**
- * Positive number only
- * @param enable Optional , use if you need to disable in derived classes
+ * Set class schema options, to be used when generating validations
+ * @template T
+ * @param {ValidationOptions} options Validation options
  */
-export function Positive(enable = true) {
-    return function(target: any, propertyKey: string) {
-        const description: FieldDescription = { positive: enable };
-        setFieldDescription(target, propertyKey, description);
-    };
-}
-
-/**
- * Negative number only
- * @param enable Optional, use if you need to disable in derived classes
- */
-export function Negative(enable = true) {
-    return function(target: any, propertyKey: string) {
-        const description: FieldDescription = { negative: enable };
-        setFieldDescription(target, propertyKey, description);
-    };
-}
-
-/**
- * Non empty array or string
- * @param enable Optional, use if you need to disable in derived classes
- */
-export function NotEmpty(enable = true) {
-    return function(target: any, propertyKey: string) {
-        const description: FieldDescription = { nonempty: enable };
-        setFieldDescription(target, propertyKey, description);
-    };
-}
-
-/**
- * Minimum length for arrays and strings
- * @param value
- */
-export function MinLength(value: number) {
-    return function(target: any, propertyKey: string) {
-        const description: FieldDescription = { minLength: value };
-        setFieldDescription(target, propertyKey, description);
-    };
-}
-
-/**
- * Specify a list of allowed values for the field
- * @param args List of allowed values
- */
-export function ValidOptions(...args: any[]) {
-    return function(target: any, propertyKey: string) {
-        const description: FieldDescription = { options: args };
-        setFieldDescription(target, propertyKey, description);
-    };
-}
-
-/**
- * Non empty arrays or strings
- * @param enable Optional, use if you need to disable in derived classes
- */
-export function Email(enable = true) {
-    return function(target: any, propertyKey: string) {
-        const description: FieldDescription = { email: enable };
-        setFieldDescription(target, propertyKey, description);
-    };
-}
-
-export function DateString(format = "YYYY-MM-DD") {
-    return function(target: any, propertyKey: string) {
-        const description: FieldDescription = { dateString: true, dateStringFormat: format };
-        setFieldDescription(target, propertyKey, description);
-    };
-}
-
-export function CustomSchema(schema: SchemaArgs) {
-    return function(target: any, propertyKey?: string) {
-        if (propertyKey) {
-            const description: FieldDescription = { customSchema: schema };
-            setFieldDescription(target, propertyKey, description);
-        } else {
-            setSchemaGlobals(target, schema);
-        }
-    };
-}
-
-export function SchemaOptions(options: ValidationOptions) {
-    return (target: any) => {
-        setSchemaOptions(target, options);
-    }
+export function SchemaOptions<T>(options: ValidationOptions) {
+  return (target: Class<T>) => {
+    setSchemaOptions(target, options);
+  };
 }
