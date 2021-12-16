076918a04a0e62c40a06e8767313d2069118fed0
hiavraham <isaakavrahamonov@icloud.com>
2021-12-16 14:20:30 +0100

    doc and properly underscore unused arguments in Class type

--- src/types/Class.ts
+++ src/types/Class.ts
@@ -1,1 +1,4 @@
-export type Class<T, Arguments extends unknown[] = []> = { new (...arguments_: Arguments): T, prototype: T };
+/**
+ * Matches a [`class`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes).
+ */
+export type Class<T, Arguments extends unknown[] = []> = { new (..._arguments: Arguments): T, prototype: T };
